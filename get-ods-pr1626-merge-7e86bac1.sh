03d1005bb81c32f1b2085a86ec611eba644e8b86
Lightheartdevs <Lightheartdevs@users.noreply.github.com>
2026-06-27 23:18:51 -0700

    test: add PR 1626 merge wrapper for 7e86bac1

---
+++ get-ods-pr1626-merge-7e86bac1.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export ODS_REPO_URL="${ODS_REPO_URL:-https://github.com/Light-Heart-Labs/DreamServer.git}"
+export ODS_REF="${ODS_REF:-codex/pr1626-merge-test-7e86bac1}"
+export ODS_BOOTSTRAP_REF="${ODS_BOOTSTRAP_REF:-$ODS_REF}"
+
+bootstrap_url="https://raw.githubusercontent.com/Light-Heart-Labs/DreamServer/${ODS_BOOTSTRAP_REF}/ods/get-ods.sh"
+curl -fsSL "$bootstrap_url" | bash -s -- "$@"
